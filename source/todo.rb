aed525eec7710e0e3028a1effacb1c9992d277d5
Nicola Beuscher <email": null>
2015-12-10 12:23:45 -0500

    Add display message method to view class

--- source/todo.rb
+++ source/todo.rb
@@ -60,9 +60,9 @@ class View
   end
 
   def display_message
-    puts "      -----------TO DO LIST----------
-    Commands: \"list\" displays your to-do list
-              \"quit\" quits the program"
+    puts "    -----------TO DO LIST----------
+    \"list\" displays your to-do list
+    \"quit\" quits the program"
   end
 
 end
