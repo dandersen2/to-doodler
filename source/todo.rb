56bb5bebd81cbbe8a2367e2a51720361272baddd
Zach <email": null>
2015-12-10 11:20:59 -0500

    Define list and task classes. List can parse items, review items and add items.

--- source/todo.rb
+++ source/todo.rb
@@ -8,3 +8,61 @@
 
 # Note that (4) is where the essence of your application lives.
 # Pretty much every application in the universe has some version of responsibilities (1), (2), and (3).
+
+require 'csv'
+require 'pry'
+
+class Task
+
+attr_reader :task_item
+
+  def initialize(task_item = {})
+    @task_item = task_item.fetch(:task_item)
+  end
+
+end
+
+
+class List
+
+attr_reader :task_file, :set_of_tasks
+# attr_writer :set_of_tasks
+
+  def initialize(task_file)
+    @task_file = task_file
+    @set_of_tasks = []
+  end
+
+  def parse_list_items_from_file
+    CSV.foreach(@task_file) {|row| @set_of_tasks << Task.new({:task_item => row[0]})}
+    @set_of_tasks
+  end
+
+  def review_todo_list
+    @set_of_tasks
+  end
+
+  def add_task_to_list(new_task)
+    @set_of_tasks << Task.new(new_task)
+  end
+
+end
+
+binding.pry
+
+puts
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ source/todo_spec.rb
@@ -0,0 +1,41 @@
+require_relative 'todo'
+
+
+describe Task do
+
+let (:task) {Task.new(task_item: "Take out the trash")}
+
+  describe 'task' do
+    it 'should read the new task' do
+      expect(task.task_item).to eq "Take out the trash"
+    end
+  end
+
+end
+
+describe List do
+
+let (:list) {List.new('todo.csv')}
+
+  describe 'task_file' do
+    it 'should take in the file passed to the class' do
+      expect(list.task_file).to eq ('todo.csv')
+    end
+  end
+
+  describe 'set_of_tasks' do
+    it 'should return an array of task items' do
+      expect(list.parse_list_items_from_file.length).to eq 13
+    end
+  end
+
+  describe 'add_task_to_list' do
+    it 'should add a task to the current array of tasks' do
+      list.parse_list_items_from_file
+      expect(list.add_task_to_list({task_item: "Take out the trash"}).length).to eq 14
+    end
+  end
+
+end
+
+
