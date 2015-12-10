4b882912916a593f9f7c97005d5d83d490e76512
Sasha <email": null>
2015-12-10 12:58:32 -0500

    List and Task classes

--- source/todo.rb
+++ source/todo.rb
@@ -8,3 +8,107 @@
 
 # Note that (4) is where the essence of your application lives.
 # Pretty much every application in the universe has some version of responsibilities (1), (2), and (3).
+require 'csv'
+require_relative 'ListParser'
+require 'byebug'
+# require 'byebug'
+
+include ListParser
+
+class List
+attr_reader :list
+
+  def initialize
+    @list = []
+  end
+
+  def add(task_string)
+    @list << Task.new(task_string)
+  end
+
+  def delete(list_index)
+    @list.delete_at(list_index - 1)
+    @list
+  end
+
+  def complete(list_index)
+    completed_task = @list[list_index - 1].task
+    @list[list_index - 1] = Task.new("<complete> " +completed_task)
+    @list
+  end
+
+  def to_s
+  @list.map.with_index{|task, task_i| "#{task_i + 1}. #{task}"}.join("\n")
+  end
+end
+
+
+class Task
+attr_reader :task
+  def initialize(task)
+    @task = task
+  end
+
+  def to_s
+    @task
+  end
+end
+
+
+  todo = List.new
+  my_task = "Visit doctor"
+
+  todo.add(my_task)
+  p todo.to_s
+  puts todo
+  puts todo.to_s.class
+  puts todo.to_s == "1. Visit doctor"
+
+
+# class Controller
+
+#   def initialize
+#     @list = List.new(build_list)
+#     @view = View.new()
+#     #run_interface
+#   end
+
+#  Parse the todo.csv file and wrap each entry in easier-to-manipulate
+# Ruby objects
+#   CSV.foreach(read in file)
+
+
+
+#   def run_interface
+#     based on input, this function is called
+#     Use command line args to take appropriate action in controller class, interpret argument as one of the functions
+
+#   Call functions to modify list
+
+#   if no input file given, default to filename todo.csv
+
+#   end
+# end
+
+
+# class View
+
+#   def get_input
+
+#   end
+
+#   def display(output)
+#     puts output
+#   end
+#       Display list to user
+#   print list to user using a puts function
+#   end
+# end
+
+
+#   Parse the command-line arguments
+#   in view class, take argument and feed to controller file
+
+
+# end
+
---
+++ source/todo_spec.rb
@@ -0,0 +1,64 @@
+require_relative 'todo.rb'
+describe List do
+  let(:task_string) {"Visit doctor"}
+  let(:new_task) {Task.new(task_string)}
+
+  describe '#initialize' do
+    it 'should create a string' do
+      expect(new_task.task).to eq (task_string)
+    end
+  end
+
+  describe '#to_s' do
+    it 'should print "Visit doctor"' do
+      expect(new_task.to_s).to eq(task_string)
+    end
+  end
+
+end
+
+
+
+describe List do
+  let(:todo) {List.new}
+  let(:my_task) {"Visit doctor"}
+
+  describe '#initialize' do
+    it 'should create an array' do
+      expect(todo.list).to eq ([])
+    end
+  end
+
+  describe '#add' do
+    it 'should add task "Visit doctor"' do
+      todo.add(my_task)
+      expect(todo.list.first.task).to eq(my_task)
+    end
+  end
+
+  describe '#delete' do
+    it 'should delete "Visit doctor task' do
+      todo.add(my_task)
+      todo.delete(1)
+      expect(todo.list.length).to eq(0)
+    end
+  end
+
+  describe '#complete' do
+    it 'should mark "Visit doctor" as complete' do
+      todo.add(my_task)
+      todo.complete(1)
+      expect(todo.list.first.task).to eq("<complete> " + my_task)
+    end
+  end
+
+  describe '#to_s' do
+    it 'should print "Visit doctor' do
+      todo.add(my_task)
+      expect(todo.to_s).to eq("1. " + my_task)
+    end
+  end
+end
+
+
+
