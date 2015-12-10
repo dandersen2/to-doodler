803fea56635e93e8fe2971b691e04d1c7818175a
Carolina Medellin <email": null>
2015-12-10 17:30:52 -0500

    still missing the controller

--- controller.rb
+++ controller.rb
@@ -3,17 +3,31 @@ require_relative 'task'
 require_relative 'list'
 require 'pry'
 class Controller
+
   attr_reader :list, :to_dos
   def initialize
     @list = List.new('source/todo.csv')
     @list.add_list
   end
-  def to_s
-    "You got so much to do! #{@list.to_dos.to_do}"
-  end
 end
-Controller.new
+todo_list = Controller.new
+# list.add({to_do: "Eat Sandwhich"})
 binding.pry
 p Controller
 
 
+# list = List.new('source/todo.csv')
+# list.add_list
+
+# list.add({to_do: "Eat Sandwhich"})
+# # list.delete("Lose Lil to Danny")
+# list.add
+
+# #NEED THIS.. changes false to true on the status
+# list.done("Lose Lil to Danny")
+
+
+# puts list
+# binding.pry
+# # list.delete("Move with Lil to the black mountain hills of Dakota")
+# puts list
--- list.rb
+++ list.rb
@@ -32,21 +32,7 @@ attr_reader :file, :to_dos, :each_task
 
 end
 
-list = List.new('source/todo.csv')
-list.add_list
 
-list.add({to_do: "Eat Sandwhich"})
-# list.delete("Lose Lil to Danny")
-list.add
-
-#NEED THIS.. changes false to true on the status
-list.done("Lose Lil to Danny")
-
-
-puts list
-binding.pry
-# list.delete("Move with Lil to the black mountain hills of Dakota")
-puts list
 
 
 
